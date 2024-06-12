6223d1250167db2865be57bb32f023785c88118f
Martí Planas Domenech <84787020+martiplanas@users.noreply.github.com>
2024-06-12 09:35:06 +0200

    asdas

--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -5,6 +5,16 @@ var WindTurbineScene = preload("res://Scenes/Placeholders/wind_turbine.tscn")
 
 const GRID_SIZE = 32  # Adjust this value to your grid size
 
+var occupied_positions = {}
+
+#---Buildings Avalible---
+var wt_avalible = 0
+var sp_avalible = 0
+var nc_avalible = 0
+
+#0=nobuilding/1=wt/2=sp/3=nc
+var buildingSelected = 0
+
 func _ready():
 	set_process_input(true)
 
@@ -14,13 +24,42 @@ func _input(event):
 			place_building(event.position)
 
 func place_building(position):
-	var new_building = WindTurbineScene.instantiate()
-	print("Building created on", str(position))
-	add_child(new_building)
-	new_building.position = snap_to_grid(position)
+	if buildingSelected != 0:
+		var grid_position = snap_to_grid(position)
+		if grid_position not in occupied_positions && !is_mouse_over_ui():
+			if buildingSelected == 1:
+				if wt_avalible >= 1:
+					var new_building = WindTurbineScene.instantiate()
+					wt_avalible -= 1
+					print("Building created on", str(position))
+					add_child(new_building)
+					new_building.position = grid_position
+					occupied_positions[grid_position] = new_building
+		else:
+			print("Place ocupied")
 
 func snap_to_grid(position):
 	# Calculate the nearest grid point
 	position.x = round(position.x / GRID_SIZE) * GRID_SIZE
 	position.y = round(position.y / GRID_SIZE) * GRID_SIZE
 	return position
+
+func _on_mousemode_pressed():
+	buildingSelected = 0
+	print("Building Selected: ", buildingSelected)
+
+func _on_wind_turbine_build_pressed():
+	buildingSelected = 1
+	print("Building Selected: ", buildingSelected)
+
+func _on_solar_panel_build_pressed():
+	buildingSelected = 0
+	#print("Building Selected: ", buildingSelected)
+	wt_avalible += 1
+
+func is_mouse_over_ui():
+	var ui_elements = get_tree().get_nodes_in_group("ui")
+	for element in ui_elements:
+		if element is Button and element.get_rect().has_point(element.get_global_mouse_position()):
+			return true
+	return false
