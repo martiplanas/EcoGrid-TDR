d674a50d74b96b98e75fcfb6db1c76755a091006
Martí Planas Domenech <84787020+martiplanas@users.noreply.github.com>
2024-07-03 16:59:46 +0200

    new stuff

---
+++ MainMenuController.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+func _on_dailly_challange_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
