9e72defcfdad2e3ab18109cc4cdb0634b9a0bd44
Martí Planas Domenech <84787020+martiplanas@users.noreply.github.com>
2024-07-06 23:45:06 +0200

    new map + camera limits beta

--- Scripts/CameraControler.gd
+++ Scripts/CameraControler.gd
@@ -5,8 +5,8 @@ extends Camera2D
 
 # Zoom variables
 @export var zoom_speed: float = 0.01
-@export var min_zoom: float = 0.1
-@export var max_zoom: float = 0.7
+@export var min_zoom: float = 0.15
+@export var max_zoom: float = 0.6
 
 @onready var ui = $Control
 
@@ -33,3 +33,6 @@ func _process(delta: float) -> void:
 		zoom = (zoom + Vector2.ONE * zoom_speed).clamp(Vector2.ONE * min_zoom, Vector2.ONE * max_zoom)
 	
 	scale = Vector2(1 / zoom.x, 1 / zoom.y)
+	
+	position.x = clamp(position.x, -1000, 5000)
+	position.y = clamp(position.y, -1000, 5000)
